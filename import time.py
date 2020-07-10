dc0a97d17a082a4fa6773ca835fffc482619d9d0
Sergey Leksikov <lexikovs@gmail.com>
2020-07-10 13:23:31 +0900

    tested the linter

---
+++ import time.py
@@ -0,0 +1,15 @@
+import time
+a = 1
+b = 2
+print(time.time())
+print("Hello", a)
+
+long_really_longoooooookkkkkkkkkkkkkkkkkkkkjjjjjjjjjjVariable = 2
+
+
+def test(a: int) -> int:
+    print(a)
+    return 1
+
+
+test(a)
