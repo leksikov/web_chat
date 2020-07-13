a74048d715e31c749ac446cc9ded47ce8979dbf2
Sergey Leksikov <lexikovs@gmail.com>
2020-07-13 13:43:40 +0900

    adding more functions

--- server.py
+++ server.py
@@ -5,26 +5,42 @@ from aiohttp import web
 # tutorial for pyenv
 # https://hackernoon.com/reaching-python-development-nirvana-bb5692adf30c
 # https://dev.to/writingcode/the-python-virtual-environment-with-pyenv-pipenv-3mlo
-
+"""
 print("\n\n\n\n")
 print(sys.executable)
 print()
 print(sys.version)
 print(sys.path)
-
-
+"""
 
 
 async def hello(request):
-    return web.Response(text="hello world Seryoga")
-
-
-def init_func(argv):
-    app = web.Application()
-    app.router.add_get("/", index_handler)
-    return app
     
+    return web.Response(text="Hello enter your user name:")
+
+
+async def login(request):
+    param1 = request.rel_url.query['user_name']
+
+    # insert user name and session details into redis db
+
+    return web.Response(text=f"Hello {param1}")
+
+
+async def chatroom(request):
+
+    # display recent chat messages
+    return web.Response(text="Chat room")
+
+async def message(request):
+
+    # send message and insert to redis db
+    return web.Response(text="Chat room")
+
+
 app = web.Application()
-app.add_routes([web.get('/', hello)])
+app.add_routes([web.get('/', hello),
+                web.get('/login', login),
+                web.get('/chatroom', chatroom)])
 
 web.run_app(app)
